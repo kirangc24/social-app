2573700ae1177812ff589e7d96d13bc862c08855
kirangc24 <106902704+kirangc24@users.noreply.github.com>
2024-10-21 19:39:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 
 // Create a connection to the database
 const db = mysql.createConnection({
-    host: 'mysql',
+    host: 'db',
     user: 'root',
     password: 'MySecureP@ss1',
     database: 'social_app'
